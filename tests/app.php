2eb9f3b5d32c7b6085c08688c36f88b54b72e864
ElisDN <mail@elisdn.ru>
2015-09-30 22:28:41 +0300

    Step - First User tests

--- tests/app.php
+++ tests/app.php
@@ -6,9 +6,37 @@ use app\models\User;
 
 require(__DIR__ . '/_bootstrap.php');
 
-$user = new User();
+class UserTest
+{
+    public function testValidateEmptyValues()
+    {
+        $user = new User();
 
-$user->username = 'ElisDN';
-$user->email = 'mail@elisdn.ru';
+        echo 'validate empty username and email';
+        if ($user->validate() == false) {
+            echo ' Ok' . PHP_EOL;
+        } else {
+            echo ' Fail' . PHP_EOL;
+            exit();
+        }
 
-print_r($user->getAttributes());
+        echo 'check empty username error';
+        if (array_key_exists('username', $user->getErrors())) {
+            echo ' Ok' . PHP_EOL;
+        } else {
+            echo ' Fail' . PHP_EOL;
+            exit();
+        }
+
+        echo 'check empty email error';
+        if (array_key_exists('email', $user->getErrors())) {
+            echo ' Ok' . PHP_EOL;
+        } else {
+            echo ' Fail' . PHP_EOL;
+            exit();
+        }
+    }
+}
+
+$test = new UserTest();
+$test->testValidateEmptyValues();
