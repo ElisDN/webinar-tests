45ead380e6f4a890c916454daf674e9ede100e7f
ElisDN <mail@elisdn.ru>
2015-09-30 23:10:55 +0300

    Step - Scandir

--- tests/app.php
+++ tests/app.php
@@ -4,15 +4,20 @@ namespace tests;
 
 require(__DIR__ . '/_bootstrap.php');
 
-$class = new \ReflectionClass('\tests\unit\UserTest');
-foreach($class->getMethods() as $method) {
-    if (substr($method->name, 0, 4) == 'test') {
-        echo 'Test ' . $method->class . '::' . $method->name . PHP_EOL . PHP_EOL;
-        /** @var TestCase $test */
-        $test = new $method->class;
-        $test->setUp();
-        $test->{$method->name}();
-        $test->tearDown();
-        echo PHP_EOL;
+foreach (scandir(__DIR__ . '/unit') as $file) {
+    if (substr($file, -8, 8) == 'Test.php') {
+        $className = pathinfo($file, PATHINFO_FILENAME);
+        $class = new \ReflectionClass('\tests\unit\\' . $className);
+        foreach($class->getMethods() as $method) {
+            if (substr($method->name, 0, 4) == 'test') {
+                echo 'Test ' . $method->class . '::' . $method->name . PHP_EOL . PHP_EOL;
+                /** @var TestCase $test */
+                $test = new $method->class;
+                $test->setUp();
+                $test->{$method->name}();
+                $test->tearDown();
+                echo PHP_EOL;
+            }
+        }
     }
 }
