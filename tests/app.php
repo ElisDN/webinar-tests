643500e49a6c8139be443d497d1e2ab9ac9c048a
ElisDN <mail@elisdn.ru>
2015-09-30 22:52:54 +0300

    Step - Added unit folder

--- tests/app.php
+++ tests/app.php
@@ -2,79 +2,9 @@
 
 namespace tests;
 
-use app\models\User;
-use Yii;
-
 require(__DIR__ . '/_bootstrap.php');
 
-class UserTest extends TestCase
-{
-    public function setUp()
-    {
-        parent::setUp();
-
-        User::deleteAll();
-        Yii::$app->db->createCommand()->insert(User::tableName(), [
-            'username' => 'user',
-            'email' => 'user@email.com',
-        ])->execute();
-    }
-
-    public function testValidateEmptyValues()
-    {
-        $user = new User();
-
-        $this->assertFalse($user->validate(), 'validate empty username and email');
-        $this->assertArrayHasKey('username', $user->getErrors(), 'check empty username error');
-        $this->assertArrayHasKey('email', $user->getErrors(), 'check empty email error');
-    }
-
-    public function testValidateWrongValues()
-    {
-        $user = new User([
-            'username' => 'Wrong % Username',
-            'email' => 'wrong_email',
-        ]);
-
-        $this->assertFalse($user->validate(), 'validate incorrect username and email');
-        $this->assertArrayHasKey('username', $user->getErrors(), 'check incorrect username error');
-        $this->assertArrayHasKey('email', $user->getErrors(), 'check incorrect email error');
-    }
-
-    public function testValidateExistedValues()
-    {
-        $user = new User([
-            'username' => 'user',
-            'email' => 'user@email.com',
-        ]);
-
-        $this->assertFalse($user->validate(), 'model is not valid');
-        $this->assertArrayHasKey('username', $user->getErrors(), 'check existed username error');
-        $this->assertArrayHasKey('email', $user->getErrors(), 'check existed email error');
-    }
-
-    public function testValidateCorrectValues()
-    {
-        $user = new User([
-            'username' => 'CorrectUsername',
-            'email' => 'correct@email.com',
-        ]);
-
-        $this->assertTrue($user->validate(), 'correct model is valid');
-    }
-
-    public function testSaveIntoDatabase()
-    {
-        $user = new User([
-            'username' => 'TestUsername',
-            'email' => 'test@email.com',
-        ]);
-
-        $this->assertTrue($user->save(), 'model is saved');
-    }
-}
-
-$class = new \ReflectionClass('\tests\UserTest');
+$class = new \ReflectionClass('\tests\unit\UserTest');
 foreach($class->getMethods() as $method) {
     if (substr($method->name, 0, 4) == 'test') {
         echo 'Test ' . $method->class . '::' . $method->name . PHP_EOL . PHP_EOL;
