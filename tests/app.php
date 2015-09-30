8df54c3e825c0c8b201a52549f6da80e05f26f32
ElisDN <mail@elisdn.ru>
2015-09-30 22:43:56 +0300

    Step - Existed data conflicts

--- tests/app.php
+++ tests/app.php
@@ -8,35 +8,14 @@ require(__DIR__ . '/_bootstrap.php');
 
 class UserTest extends TestCase
 {
-    public function testValidateEmptyValues()
-    {
-        $user = new User();
-
-        $this->assertFalse($user->validate(), 'validate empty username and email');
-        $this->assertArrayHasKey('username', $user->getErrors(), 'check empty username error');
-        $this->assertArrayHasKey('email', $user->getErrors(), 'check empty email error');
-    }
-
-    public function testValidateWrongValues()
+    public function testSaveIntoDatabase()
     {
         $user = new User([
-            'username' => 'Wrong % Username',
-            'email' => 'wrong_email',
+            'username' => 'TestUsername',
+            'email' => 'test@email.com',
         ]);
 
-        $this->assertFalse($user->validate(), 'validate incorrect username and email');
-        $this->assertArrayHasKey('username', $user->getErrors(), 'check incorrect username error');
-        $this->assertArrayHasKey('email', $user->getErrors(), 'check incorrect email error');
-    }
-
-    public function testValidateCorrectValues()
-    {
-        $user = new User([
-            'username' => 'CorrectUsername',
-            'email' => 'correct@email.com',
-        ]);
-
-        $this->assertTrue($user->validate(), 'correct model is valid');
+        $this->assertTrue($user->save(), 'model is saved');
     }
 }
 
