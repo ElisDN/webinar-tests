3595d68b2ad640b1fdaded8d97c34e631b172a6f
ElisDN <mail@elisdn.ru>
2015-09-30 22:22:21 +0300

    Step - Create application

--- tests/app.php
+++ tests/app.php
@@ -1,3 +1,15 @@
 <?php
 
-echo 'Hello!' . PHP_EOL;
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'test');
+
+require(__DIR__ . '/../vendor/autoload.php');
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
+
+$config = require(__DIR__ . '/../config/web.php');
+
+$application = new yii\web\Application($config);
+
+echo Yii::$app->name . PHP_EOL;
