95819c934e1ccd44674da0ccb19b3cdaa55533df
ElisDN <mail@elisdn.ru>
2015-09-30 22:29:31 +0300

    Step - Assert method

--- tests/app.php
+++ tests/app.php
@@ -8,33 +8,24 @@ require(__DIR__ . '/_bootstrap.php');
 
 class UserTest
 {
+    protected function assert($condition, $message = '')
+    {
+        echo $message;
+        if ($condition) {
+            echo ' Ok' . PHP_EOL;
+        } else {
+            echo ' Fail' . PHP_EOL;
+            exit();
+        }
+    }
+
     public function testValidateEmptyValues()
     {
         $user = new User();
 
-        echo 'validate empty username and email';
-        if ($user->validate() == false) {
-            echo ' Ok' . PHP_EOL;
-        } else {
-            echo ' Fail' . PHP_EOL;
-            exit();
-        }
-
-        echo 'check empty username error';
-        if (array_key_exists('username', $user->getErrors())) {
-            echo ' Ok' . PHP_EOL;
-        } else {
-            echo ' Fail' . PHP_EOL;
-            exit();
-        }
-
-        echo 'check empty email error';
-        if (array_key_exists('email', $user->getErrors())) {
-            echo ' Ok' . PHP_EOL;
-        } else {
-            echo ' Fail' . PHP_EOL;
-            exit();
-        }
+        $this->assert($user->validate() == false, 'model is not valid');
+        $this->assert(array_key_exists('username', $user->getErrors()), 'check username error');
+        $this->assert(array_key_exists('email', $user->getErrors()), 'check email error');
     }
 }
 
