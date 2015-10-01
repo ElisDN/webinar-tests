6b8619184a8fc29f239e4703bb00438b76165b9b
ElisDN <mail@elisdn.ru>
2015-10-01 12:13:01 +0300

    Step - Test Search form

--- tests/acceptance/SearchCept.php
+++ tests/acceptance/SearchCept.php
@@ -7,3 +7,9 @@ $I->wantTo('ensure that search works');
 
 $I->amOnPage('/');
 $I->see('Найти');
+
+$I->fillField('input[name=text]', 'Codeception');
+$I->click('button[type=submit]');
+
+$I->seeInCurrentUrl('/search/?text=Codeception');
+$I->see('Codeception - BDD-style PHP testing');
