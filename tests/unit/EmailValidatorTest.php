c64f3f35db8fe963cbec46aed19fd9ee5685e295
ElisDN <mail@elisdn.ru>
2015-10-01 11:26:01 +0300

    Step - Extract variants

--- tests/unit/EmailValidatorTest.php
+++ tests/unit/EmailValidatorTest.php
@@ -16,10 +16,18 @@ class EmailValidatorTest extends Test
     {
         $validator = new EmailValidator();
 
-        $this->assertEquals($validator->validate('mail@site.com'), true);
-        $this->assertEquals($validator->validate('mail.dot@site.com'), true);
-        $this->assertEquals($validator->validate('mail_site.com'), false);
-        $this->assertEquals($validator->validate('mail@site'), false);
-        $this->assertEquals($validator->validate('mail@123'), false);
+        $variants = [
+            ['mail@site.com', true],
+            ['mail.dot@site.com', true],
+            ['mail_site.com', false],
+            ['mail@site', false],
+            ['mail@123', false],
+        ];
+
+        foreach ($variants as $variant) {
+            $email = $variant[0];
+            $result = $variant[1];
+            $this->assertEquals($validator->validate($email), $result);
+        }
     }
 }
