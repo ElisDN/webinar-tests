1957aa4a9e028b30ed59482e08a9a185414a4d40
ElisDN <mail@elisdn.ru>
2015-10-01 11:40:58 +0300

    Step - Empty tests

---
+++ tests/unit/FutureTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace tests\unit;
+
+use Codeception\TestCase\Test;
+
+class FutureTest extends Test
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    public function testSomeFuture()
+    {
+
+    }
+
+    public function testOtherFuture()
+    {
+
+    }
+}
