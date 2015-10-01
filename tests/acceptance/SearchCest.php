ed28e977c54138a1132888d7cae0a60ecc930412
ElisDN <mail@elisdn.ru>
2015-10-01 12:21:41 +0300

    Step - Acceptance Cest
    codecept generate:cest acceptance Search

---
+++ tests/acceptance/SearchCest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace tests\acceptance;
+
+use \AcceptanceTester;
+
+class SearchCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
