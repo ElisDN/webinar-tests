d64c0c2058301c5dc8be2dc72e0c49287c55fe57
ElisDN <mail@elisdn.ru>
2015-10-01 12:23:29 +0300

    Step - Cest test

--- tests/acceptance/SearchCest.php
+++ tests/acceptance/SearchCest.php
@@ -6,16 +6,27 @@ use \AcceptanceTester;
 
 class SearchCest
 {
-    public function _before(AcceptanceTester $I)
-    {
-    }
-
-    public function _after(AcceptanceTester $I)
-    {
-    }
-
-    // tests
     public function tryToTest(AcceptanceTester $I)
     {
+        $I->wantTo('ensure that search works');
+
+        $I->amOnPage('/');
+
+        $I->see('Найти');
+
+        $I->fillField('input[name=text]', 'Codeception');
+
+        if (method_exists($I, 'wait')) {
+            $I->wait(1); // only for selenium
+        }
+
+        $I->click('button[type=submit]');
+
+        if (method_exists($I, 'wait')) {
+            $I->wait(3); // only for selenium
+        }
+
+        $I->seeInCurrentUrl('/search/?text=Codeception');
+        $I->see('Codeception - BDD-style PHP testing');
     }
 }
