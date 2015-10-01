2c4e47f787752c5b4a8a460e4b4c17b231685c9e
ElisDN <mail@elisdn.ru>
2015-10-01 10:38:09 +0300

    Step - Own UserTest

--- tests/unit/UserTest.php
+++ tests/unit/UserTest.php
@@ -1,23 +1,77 @@
 <?php
 
-class UserTest extends \Codeception\TestCase\Test
+namespace tests\unit;
+
+use app\models\User;
+use Codeception\TestCase\Test;
+use Yii;
+
+class UserTest extends Test
 {
     /**
      * @var \UnitTester
      */
     protected $tester;
 
-    protected function _before()
+    public function _before()
     {
+        User::deleteAll();
+        Yii::$app->db->createCommand()->insert(User::tableName(), [
+            'username' => 'user',
+            'email' => 'user@email.com',
+        ])->execute();
     }
 
-    protected function _after()
+    public function testValidateEmptyValues()
     {
+        $user = new User();
+
+        $this->assertFalse($user->validate(), 'validate empty username and email');
+        $this->assertArrayHasKey('username', $user->getErrors(), 'check empty username error');
+        $this->assertArrayHasKey('email', $user->getErrors(), 'check empty email error');
     }
 
-    // tests
-    public function testMe()
+    public function testValidateWrongValues()
     {
+        $user = new User([
+            'username' => 'Wrong % Username',
+            'email' => 'wrong_email',
+        ]);
 
+        $this->assertFalse($user->validate(), 'validate incorrect username and email');
+        $this->assertArrayHasKey('username', $user->getErrors(), 'check incorrect username error');
+        $this->assertArrayHasKey('email', $user->getErrors(), 'check incorrect email error');
+    }
+
+    public function testValidateExistedValues()
+    {
+        $user = new User([
+            'username' => 'user',
+            'email' => 'user@email.com',
+        ]);
+
+        $this->assertFalse($user->validate(), 'model is not valid');
+        $this->assertArrayHasKey('username', $user->getErrors(), 'check existed username error');
+        $this->assertArrayHasKey('email', $user->getErrors(), 'check existed email error');
+    }
+
+    public function testValidateCorrectValues()
+    {
+        $user = new User([
+            'username' => 'CorrectUsername',
+            'email' => 'correct@email.com',
+        ]);
+
+        $this->assertTrue($user->validate(), 'correct model is valid');
+    }
+
+    public function testSaveIntoDatabase()
+    {
+        $user = new User([
+            'username' => 'TestUsername',
+            'email' => 'test@email.com',
+        ]);
+
+        $this->assertTrue($user->save(), 'model is saved');
     }
 }
