2ed0df7252489902e9dd08b05678b8654f81d7f9
ElisDN <mail@elisdn.ru>
2015-10-01 10:28:50 +0300

    Step - Codecept bootstrap

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,17 +1,2 @@
 <?php
-
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'test');
-
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
-
-Yii::setAlias('@tests', dirname(__DIR__) . '/tests');
-
-$config = \yii\helpers\ArrayHelper::merge(
-    require(__DIR__ . '/../config/web.php'),
-    require(__DIR__ . '/config/config.php')
-);
-
-$application = new yii\web\Application($config);
- 
+// This is global bootstrap for autoloading
